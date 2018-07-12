34ca9aa9703f07ee8b3da0ce3815bd889c6ddd04
Codevotee <codevotee@gmail.com>
2018-07-12 19:14:14 +0100

    enabled the removal of finished to-dos

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@
 $("li").click(function(){
 	$(this).toggleClass("checked");
 });
+
+// click on "X" to delete to-do
+
+$("span").click(function(event){
+	// console.log("ta-hee"); for testing purposes
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove(); // "this" refers to the parent element, not the span
+	}); // removes the li which the span is contained in
+	event.stopPropagation(); 
+	// prevents any events firing after the current event (event bubbling) e.g. toggling the "checked" class
+});
